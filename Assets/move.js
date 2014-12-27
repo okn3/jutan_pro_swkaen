c07888cff5b45d53a53cdc2caaf6fa4463e35e9a
甘利聡 <swkaen@amarisatoshi-no-MacBook-Pro.local>
2014-12-27 20:36:45 +0900

    aaaa

---
+++ Assets/move.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+var vy :float=0;
+var rotate_rate :float=0.5;
+//0.0055*90=0.5
+function Start () {
+
+}
+
+function Update () {
+	vy=Input.GetAxis("Vertical");
+	transform.Translate(0, vy*0.5,0.5);
+
+//上下移動で視点変更
+	 //  if (Input.GetKey("up")) {
+  //   // transform.position.y += spd*100;
+  //   transform.Rotate(-5,0,0);
+  // }
+  // else if (Input.GetKey("down")) {
+  //   // transform.position.y -= spd*100;
+  //   transform.Rotate(15,0,0);
+  // }
+
+	if(Input.GetKey("right")){
+	    transform.Rotate(0,rotate_rate,0);
+	    if(rotate_rate<1){
+			// rotate_rate+=0.0059;
+      rotate_rate+=0.05;
+		}
+	}else if(Input.GetKey("left")){
+		transform.Rotate(0,-rotate_rate,0);
+		if(rotate_rate<1){
+			// rotate_rate+=0.0059;
+      rotate_rate+=0.05;
+		}
+	}
+	if(Input.GetKeyUp("right")||Input.GetKeyUp("left")){
+		rotate_rate = 0.5;
+	}
+
+
+
+}
---
+++ Assets/move_player.js
@@ -0,0 +1,51 @@
+﻿
+
+#pragma strict
+
+var spd:float = 3;
+var i:float;
+function Start () {
+
+}
+
+function Update () {
+  transform.Translate(0,0,1); //直進
+
+  if (Input.GetKey("right")){
+    transform.Rotate(0,1,-3);
+  }else if(Input.GetKey("left")){
+    transform.Rotate(0,-1,3);
+  }
+
+  if (Input.GetKey("up")) {
+    transform.position.y += spd*100;
+    transform.Rotate(-5,0,0);
+  }
+  else if (Input.GetKey("down")) {
+    transform.position.y -= spd*100;
+    transform.Rotate(15,0,0);
+  }
+}
+
+
+// 前回
+  // transform.position.x += spd*30; //直進
+  // // transform.right.x += spd*100;
+
+  // if (Input.GetKey("right")) {
+  //   transform.position.z -= spd*100;
+  //   // transform.rotation.z += spd*10;
+  // }
+  // else if (Input.GetKey("left")) {
+  //   transform.position.z += spd*100;
+  //   // transform.rotation.z -= spd*10;
+  // }
+
+
+  // if (Input.GetKey("up")) {
+  //   transform.position.y += spd*100;
+  // }
+  // else if (Input.GetKey("down")) {
+  //   transform.position.y -= spd*100;
+  // }
+
